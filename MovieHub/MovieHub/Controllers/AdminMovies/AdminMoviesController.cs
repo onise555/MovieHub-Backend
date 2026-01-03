9c9f9a66b7d6c03a5d7eb78d6e4b53031e319799
Onise <tsotskhalashvili558@gmail.com>
2026-01-03 22:48:07 +0400

    Add Movie Controller

---
+++ MovieHub/MovieHub/Controllers/AdminMovies/AdminMoviesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieHub.Data;
+using MovieHub.Models.Movies;
+using MovieHub.Requests.AdminMoviesRequests;
+
+namespace MovieHub.Controllers.AdminMovies
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminMoviesController : ControllerBase
+    {
+        private readonly DataContext _data;
+
+        public AdminMoviesController(DataContext data)
+        {
+            _data = data;
+        }
+
+
+        [HttpPost("Add-Movies")]
+        public ActionResult AddMovie(CreateMovieRequest req)
+        {
+            Movie movie = new Movie()
+            {
+               MovieName = req.MovieName,
+               CoverImg = req.CoverImg, 
+               ReleaseYear = req.ReleaseYear,   
+               Rating = req.Rating,
+               CreatedAt = req.CreatedAt,
+               UpdatedAt = req.UpdatedAt,   
+              
+            };
+
+            _data.movies.Add(movie);
+            _data.SaveChanges();
+
+            return Ok(movie);   
+        }
+    }
+}
--- MovieHub/MovieHub/Requests/AdminMoviesRequests/CreateMovieRequest.cs
+++ MovieHub/MovieHub/Requests/AdminMoviesRequests/CreateMovieRequest.cs
@@ -2,5 +2,14 @@
 {
     public class CreateMovieRequest
     {
+        public string MovieName { get; set; }
+
+        public string CoverImg { get; set; }
+
+        public decimal Rating { get; set; }
+
+        public int ReleaseYear { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
     }
 }
